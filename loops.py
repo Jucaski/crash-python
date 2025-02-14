a2af12e45a81142f1e0ffc38d4db8d763e7f8311
Jucaski <thanididbefore@gmail.com>
2025-02-13 20:00:32 -0600

    numerical lists

--- loops.py
+++ loops.py
@@ -3,3 +3,4 @@ magicians = ['alice','rick','malice','linney']
 for magician in magicians:
     print(f"{magician}")
     print(f"{magician.title()} that was a great trick!")
+#forgetting a colon in the for will cause a syntax error, python interprete this as an attempt to write a more complex loop
---
+++ numerical_lists.py
@@ -0,0 +1,6 @@
+#creating numerical lists
+#range function
+
+for value in range(1,5):
+    print(value)
+#python stops when reaches whe second value so the output never contains the end value
